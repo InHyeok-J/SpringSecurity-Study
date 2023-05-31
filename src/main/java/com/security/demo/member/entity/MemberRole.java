21ff7f5b0bda30a52833efbb261c520849f1ff5e
inhyeok <ss112d86@gmail.com>
2023-05-31 16:49:11 +0900

    feat: role, resource db 수정

---
+++ src/main/java/com/security/demo/member/entity/MemberRole.java
@@ -0,0 +1,42 @@
+package com.security.demo.member.entity;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "member_roles")
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_role_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    public MemberRole(Role role, Member member) {
+        this.role = role;
+        this.member = member;
+    }
+}
--- src/main/java/com/security/demo/member/entity/Role.java
+++ src/main/java/com/security/demo/member/entity/Role.java
@@ -9,45 +9,41 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Table(name = "roles")
 @Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Member {
+public class Role {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
     private Long id;
 
-    @Column(nullable = false, unique = true)
-    private String userId;
+    @Column(unique = true, nullable = false)
+    private String roleName;
+
 
     @Column(nullable = false)
-    private String password;
+    private String roleDesc;
 
-    @Column(nullable = false, unique = true)
-    private String email;
+    @OneToMany(mappedBy = "role", cascade = {CascadeType.ALL}, orphanRemoval = true)
+    private Set<RoleAccess> roleAccessResources = new HashSet<>();
 
-    @Column(nullable = false)
-    private String name;
-
-    @OneToMany(mappedBy = "member", cascade = {CascadeType.ALL}, orphanRemoval = true)
+    @OneToMany(mappedBy = "role", cascade = {CascadeType.ALL}, orphanRemoval = true)
     private Set<MemberRole> memberRoles = new HashSet<>();
 
     @Builder
-    public Member(String userId, String password, String email, String name) {
-        this.userId = userId;
-        this.password = password;
-        this.email = email;
-        this.name = name;
+    public Role(String roleName, String roleDesc) {
+        this.roleName = roleName;
+        this.roleDesc = roleDesc;
     }
-
-    public void updatePassword(String password) {
-        this.password = password;
-    }
-
 }
